7840fa413244b0ad24179473b8e3fee1d75c3525
AwatefVicente <vawatef@hotmail.fr>
2022-10-31 21:30:03 +0100

    ouep ouep

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,16 @@
 class MainController < ApplicationController
   def team
-    
   end
 
   def contact
-
   end
+
+  def welcome
+    @first_name = params[:first_name]
+  end
+
+  def home
+    @gossip = Gossip.all
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/team', to: 'main#team'
   get '/contact', to: 'main#contact'
+  get '/welcome/:first_name', to: 'main#welcome'
+  get '/', to: 'main#home'
 end
