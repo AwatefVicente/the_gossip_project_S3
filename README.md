e414aeff0923e6b42771c18659d8673ae5ad32ac
AwatefVicente <vawatef@hotmail.fr>
2022-10-31 20:16:21 +0100

    mea culpa

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # README
 
 Hello à ceux qui auro l'occasion delire ce message lancé à la mer.
+Vous ne me croirez surement pas mais j'ai été kidnappée par des extraterrestres, oui  oui absoluement ! Un ovni est venu seposer devant ma porte. Les bonshommes ont alors toqué à ma porte et, aveuglée par la lumière de leur soucoupe volante je n'ai su réagir lorsqu'ils m'ont attrapé par le bras pour m'entrainer violemment dans leur vaisseau.
+J'ai alors traversé l'espace pendant ce qui me paraissait pour une minute, alors qu'en réalité il s'était écoulé 1 journée.
+Boila opurquoi je n'ai pu réaliser ce merveilleux projet.
+
+
 
 * Ruby version  : ruby "3.0.0"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Tag.destroy_all
+User.destroy_all
+City.destroy_all
+Gossip.destroy_all
+
+10.times do
+  city = City.create(
+    name: Faker::Movies::StarWars.planet,
+    zip_code: Faker::Address.zip_code
+  )
+end
+
+10.times do
+  user = User.create(
+    first_name: Faker::TvShows::GameOfThrones.dragon,
+    last_name: Faker::Name.last_name,
+    description: Faker::Movies::StarWars.quote,
+    email: Faker::TvShows::GameOfThrones.dragon+"@gmail.comm",
+    age: Faker::Number.within(range: 14..80),
+    city_id: rand(1..10)
+  )
+end
+
+20.times do
+  gossip = Gossip.create(
+    title: Faker::Music::Opera.mozart,
+    content: Faker::Music::Prince.lyric,
+    user_id: rand(1..10)
+  )
+end
+
+
+10.times do
+  tag = Tag.create(
+    title: Faker::Books::CultureSeries.civ,
+    gossip_id: rand(1..20)
+  )
+end
+
+puts "succesfully done"
